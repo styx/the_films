403ba80e8b7f813f4265ade4ba87db3d3ec7498a
Mikhail S. Pobolovets <styx.mp@gmail.com>
2015-01-26 10:05:54 +0300

    Annotate models
    Signed-off-by: Mikhail S. Pobolovets <styx.mp@gmail.com>

--- app/models/film.rb
+++ app/models/film.rb
@@ -1,3 +1,19 @@
+# == Schema Information
+#
+# Table name: films
+#
+#  created_at  :datetime         not null
+#  gapoif      :string           not null
+#  id          :integer          not null, primary key
+#  kind_id     :integer
+#  mins        :integer
+#  name        :string           not null
+#  second_name :string
+#  updated_at  :datetime         not null
+#  url         :string           not null
+#  year        :integer
+#
+
 class Film < ActiveRecord::Base
   belongs_to :kind
   has_and_belongs_to_many :genres
--- app/models/kind.rb
+++ app/models/kind.rb
@@ -1,3 +1,13 @@
+# == Schema Information
+#
+# Table name: kinds
+#
+#  created_at :datetime         not null
+#  id         :integer          not null, primary key
+#  name       :string           not null
+#  updated_at :datetime         not null
+#
+
 class Kind < ActiveRecord::Base
   has_many :films
 
